3561cbfa8639a8f6e5eaaf9e452033101e7008b0
XMADK006 <xmadk006@studenti.czu.cz>
2023-02-21 20:44:19 +0100

    pridej dva ctverce

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -2,15 +2,40 @@ package cz.czechitas.turtle;
 
 import cz.czechitas.turtle.engine.*;
 
+import java.awt.*;
+
 public class HlavniProgram {
-    private Turtle zofka;
+    private Turtle zofka=new Turtle();
+
 
     public void start() {
-        zofka = new Turtle();
+        ctverec(50,Color.pink);
+        zofka.penUp();
+        zofka.turnRight(90);
+        zofka.move(100);
+        zofka.penDown();
+
+        ctverec(50,Color.red);
+
     }
+
+    public void ctverec(double delkaStrany, Color barva) {
+        zofka.setPenColor(barva);
+        for (int i=0; i < 4; i++) {
+
+            zofka.move(delkaStrany);
+            zofka.turnRight(90);
+        }
+    }
+
 
     public static void main(String[] args) {
         new HlavniProgram().start();
+
+
+
+
     }
+
 
 }
